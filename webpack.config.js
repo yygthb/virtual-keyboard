4cf6ba4f1098dd52eaa5caf3e66880f7ebe7aff0
Yura Ya <yy.ainu.dev@gmail.com>
2022-05-04 16:09:04 +0300

    feat: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: './index.html',
+      title: 'Virtual Keyboard',
+      minify: {
+        collapseWhitespace: false,
+      },
+      inject: 'body'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/img/favicon.ico'),
+          to: path.resolve(__dirname, 'dist/assets/img/favicon.ico'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+      }
+    ]
+  }
+}
